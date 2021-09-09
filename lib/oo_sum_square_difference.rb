230686c72548792ade16386cfb2a47245173d699
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-09 10:39:58 -0700

    solution

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+    def initialize(limit)
+    @limit = limit
+    end
+
+    def difference()
+        (sum_then_square() - square_then_sum()).abs
+    end
+    
+    def square_then_sum()
+        (1..@limit).reduce do |sum, element|
+            sum + element ** 2
+        end
+    end
+    
+    def sum_then_square()
+        sum_array((1..@limit))**2
+    end
+    
+    
+    def sum_array(array)
+        array.reduce do |sum, element|
+            sum + element
+        end
+    end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,21 @@
-# Implement your procedural solution here!
+# Implement your procedural solution here
+def sum_square_difference(limit)
+    (sum_then_square(limit) - square_then_sum(limit)).abs
+end
+
+def square_then_sum(limit)
+    (1..limit).reduce do |sum, element|
+        sum + element ** 2
+    end
+end
+
+def sum_then_square(limit)
+    sum_array((1..limit))**2
+end
+
+
+def sum_array(array)
+    array.reduce do |sum, element|
+        sum + element
+    end
+end
